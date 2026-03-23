8e959b6315a19a3a549552502cbf07eb41d9c7a5
IuriiOnopko <marnilyp@icloud.com>
2026-03-23 14:35:42 -0700

    Added comments to MLStrategy.

--- src/main/MLStrategy.java
+++ src/main/MLStrategy.java
@@ -9,14 +9,17 @@ import java.util.Random;
 
 public class MLStrategy implements ComputerStrategy {
     
-    private final int PATTERN_SIZE = 4; // allows people to choose the amount of moves in a pattern
+    private final int PATTERN_SIZE = 4; // the amount of moves considered in a pattern
     
     private final GameLogic gameLogic;
     private final GameData gameData;
-    private final HashMap<List<Move>, int[]> moveFrequency;
-    private final Queue<Move> moveHistory;
+    private final HashMap<List<Move>, int[]> moveFrequency; // key = move history, value = frequency of the moves
+    private final Queue<Move> moveHistory; // queue of the move history
     
-
+    /**
+     * Constructor for the MLStrategy class.
+     * @param gameData the stored game data to be uploaded to the ML 
+     */
     public MLStrategy(GameData gameData) {
         this.gameLogic = new GameLogic();
         this.gameData = gameData;
@@ -24,6 +27,10 @@ public class MLStrategy implements ComputerStrategy {
         this.moveHistory = new LinkedList<>();
     }
 
+    /**
+     * Uses the move frequency to predict the next move.
+     * @return the computer's predicted move based on the move frequency
+     */
     @Override
     public Move computerStrategyMove() {
         Move computerMove;
@@ -41,6 +48,11 @@ public class MLStrategy implements ComputerStrategy {
         return computerMove;
     }
 
+    /**
+     * Adds the move history of the round to the queue and updates the move frequency.
+     * @param humanMove the human player's move
+     * @param computerMove the computer player's move
+     */
     @Override
     public void addGameRoundHistory(Move humanMove, Move computerMove) {
 
@@ -50,11 +62,17 @@ public class MLStrategy implements ComputerStrategy {
             int[] freq = moveFrequency.computeIfAbsent(key, k -> new int[3]); // create a new array if the pattern is not found
             freq[humanMove.ordinal()]++; // increment the frequency of the human move RPS order (0 = Rock, 1 = Paper, 2 = Scissors)
         }
-
+        // Safely update the move history queue by adding the new moves
         updateMoveHistory(humanMove);
         updateMoveHistory(computerMove);        
     }
-
+    
+    /**
+     * Looks up the expected player move based on the move frequency. 
+     * If the pattern is not found, returns null.
+     * @param key the key of the move history (list of moves)
+     * @return the expected player move based on the move frequency
+     */
     private Move getExpectedPlayerMove(List<Move> key) {
         int[] freq = moveFrequency.get(key);
         if (freq == null) return null;
@@ -72,11 +90,19 @@ public class MLStrategy implements ComputerStrategy {
     }
 
     
-
+    /**
+     * Builds the key of the move history.
+     * @return a list of the last 4 moves in the move history
+     */
     private List<Move> buildKey() {
         return new ArrayList<>(moveHistory);
     }
     
+    /**
+     * Updates the move history queue by adding the new move and removing the oldest move if the queue is full.
+     * Utilizes PATTERN_SIZE to determine the size of the queue.
+     * @param move the new move to be added to the move history
+     */
     private void updateMoveHistory(Move move) {
         moveHistory.add(move);
         if (moveHistory.size() > PATTERN_SIZE) {
@@ -84,10 +110,17 @@ public class MLStrategy implements ComputerStrategy {
         }
     }
 
+    /**
+     * Generates a random move.
+     * @return a random move
+     */
     private Move getRandomMove() {
         return Move.convertToMove(new Random().nextInt(3));
     }
 
+    /**
+     * Saves the move frequency to the game data.
+     */
     @Override
     public void saveData() {
         gameData.saveData(moveFrequency);
