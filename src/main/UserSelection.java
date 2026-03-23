9be90b323fdcba6d1ae7e9dd1e33b75f84aaee43
K G <ken.garcia1019@gmail.com>
2026-03-23 13:29:59 -0700

    Update to UserSelection.java

--- src/main/UserSelection.java
+++ src/main/UserSelection.java
@@ -3,16 +3,29 @@ package main;
 import java.util.Scanner;
 
 public class UserSelection {
+
     public static ComputerStrategy getComputerStrategy(Scanner scanner, GameData gameData) {
         System.out.println("Please Select a mode:\n-r = (Random Computer Choice)\n-m = (Machine Learning Choice)");
         while (true) { 
             System.out.print("Your Choice: ");
             String userChoice = scanner.nextLine();
-            if (userChoice.equalsIgnoreCase("-r" ) || userChoice.equalsIgnoreCase("-m")){
-                return ComputerStrategyFactory.createStrategy(userChoice.toLowerCase(), gameData);
+            String validate = verifyInput(userChoice);
+            if (validate != null){
+                return ComputerStrategyFactory.createStrategy(userChoice, gameData);
             } else {
                 System.out.println("\nPlease Try Again.");
             }
+        }
+    }
+
+    public static String verifyInput(String userChoice) {
+        if (userChoice.equalsIgnoreCase("-r" ) || userChoice.equalsIgnoreCase("r")) {
+            return "-r";
+        } else if (userChoice.equalsIgnoreCase("-m" ) || userChoice.equalsIgnoreCase("m")){
+            return "-m";
+
+        } else {
+            return null;
         }
     }
 }
